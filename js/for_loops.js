feb75e8f1f52eda94774e8fb86eba540614e2add
Abraham Garcia <aberaham.garcia55@gmail.com>
2023-09-14 16:25:13 -0500

    completed step 4 for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,15 +10,18 @@
 // showMultiplicationTable(7);
 // showMultiplicationTable(5);
 
-for (let i = 1 ; i <= 10; i++) {
+//for (let i = 1 ; i <= 10; i++) {
     //generate a random number between 20 and 200
     // print out odd or even
-let max = 200;
-let min = 20;
-let random =Math.trunc( Math.random() * (max - min) + min);
-    if (random % 2 === 0) {
-    console.log(random + " is Even");}
-    if (random %2 !== 0) {
-        console.log(random + " is Odd");
-    }
+//let max = 200;
+// let min = 20;
+// let random =Math.trunc( Math.random() * (max - min) + min);
+//     if (random % 2 === 0) {
+//     console.log(random + " is Even");}
+//     if (random %2 !== 0) {
+//         console.log(random + " is Odd");
+//     }
+// }
+for (let i = 1; i < 10; i++) {
+    console.log(("" + i).repeat(i));
 }
