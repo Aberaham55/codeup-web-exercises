3d05f853937a721823a1ebf062106f235d7d74be
Abraham Garcia <aberaham.garcia55@gmail.com>
2023-11-05 13:45:09 -0600

    completed weather map

--- js/weather.js
+++ js/weather.js
@@ -1,24 +1,23 @@
 
-mapboxgl.accessToken = MAPBOX_API_KEY;
+mapboxgl.accessToken = MAP_API_TOKEN;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
     zoom: 10, // starting zoom
-    center: [-98.4916, 29.4252] // [lng, lat]
+    center: [-98.4916, 29.4252]
 });
 let marker = new mapboxgl.Marker( {
     draggable: true
 })
-    .setLngLat([-98.48, 29.42])
+    .setLngLat([-98.49468990478387, 29.427650642342783])
     .addTo(map);
 
 const onDragUpdateWeather = () => {
     const lngLat = marker.getLngLat();
     const [lng, lat] = Object.values(lngLat);
-    // function get and loops
     console.log([lng, lat])
     forecastWeatherCards(lng,lat);
-}// end of function onDragEnd
+}
 
 marker.on('dragend', onDragUpdateWeather);
 
@@ -38,8 +37,9 @@ geocoder.on('result', function(event) {
 //end of search bar //
 
 
+
 const weatherCardsDiv = document.querySelector('#weatherCardsDiv');
-    function forecastWeatherCards(lng = -98.48948239256946, lat = 29.426825118534886) {
+    function forecastWeatherCards(lng = -98.49468990478387, lat = 29.427650642342783) {
         fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${OPEN_WEATHER_API_KEY}&units=imperial`)
             .then(data => data.json())
             .then(currentWeather => {
@@ -56,7 +56,7 @@ const weatherCardsDiv = document.querySelector('#weatherCardsDiv');
                     const highTemp = weather.main.temp_max;
                     const lowTemp = weather.main.temp_min;
                     const description = weather.weather[0].description;
-                     const icon = weather.weather[0].icon;
+                    const icon = weather.weather[0].icon;
                     const windSpeed = weather.wind.speed;
                     const feelsLike = weather.main.feels_like;
 
@@ -65,7 +65,7 @@ const weatherCardsDiv = document.querySelector('#weatherCardsDiv');
                         const cardDiv = document.createElement("div");
                         cardDiv.classList.add('card');
                         cardDiv.innerHTML = `
-                          <img src=" https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" class="card-img-top " alt="...">
+                          <img src=" https://openweathermap.org/img/wn/${icon}@2x.png" class="card-img-top " alt="...">
                           <div class="card-body">
                           <h5 class="card-title">${day}, ${date}</h5>
                           <p class="main-temp">${temp}</p>    
